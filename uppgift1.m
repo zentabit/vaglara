58ab24a7a9a45630f3e44452368a87515c26e6b2
Emil Babayev <zentabit@gmail.com>
2020-03-26 15:34:58 +0100

    update uppg1

--- uppgift1.m
+++ uppgift1.m
@@ -3,7 +3,7 @@
 a = pi/6;
 n = 1.5;
 
-% härledd för hand + s211 i boken
+% h??rledd f??r hand + s211 i boken
 d = @(x) x + asin(n.*sin(a - asin(sin(x)./n))) - a;
 
 x = linspace(0, pi/2);
@@ -20,14 +20,15 @@ a2 = 10.72;
 b1 = 1.653;
 b2 = 10.27;
 
-% beräkna brytningsindex
+% ber??kna brytningsindex
 n = @(l) a1 + a2./l;
-% dess våglängder kikar vi på
+m = @(l) a1 + a2./l;
+% dess v??gl??ngder kikar vi p??
 ls = [450 540 600 700];
-% beräkna n för dessa
+% ber??kna n f??r dessa
 ns = n(ls)
-
-% ny funktion där man kan ändra n
+ms = m(ls)
+% ny funktion d??r man kan ??ndra n
 d1 = @(x, n) x + asin(n.*sin(a - asin(sin(x)./n))) - a;
 
 y1 = d1(x, ns(1));
@@ -35,8 +36,30 @@ y2 = d1(x, ns(2));
 y3 = d1(x, ns(3));
 y4 = d1(x, ns(4));
 
+z1 = d1(x, ms(1));
+z2 = d1(x, ms(2));
+z3 = d1(x, ms(3));
+z4 = d1(x, ms(4));
+
+tiledlayout(2,1)
+ax1 = nexttile;
+ax1.Title.String = "Zinkdopat kronglas"
+ax1.XLabel.String = "Infallsvinkel \theta rad"
+ax1.YLabel.String = "Avl?nkningsvinkel \delta rad"
 hold on
 plot(x, y1, 'b-')
 plot(x, y2, 'g-')
 plot(x, y3, 'y-')
 plot(x, y4, 'r-')
+hold off
+
+ax2 = nexttile;
+ax2.Title.String = "Tungt flintglas"
+ax2.XLabel.String = "Infallsvinkel \theta rad"
+ax2.YLabel.String = "Avl?nkningsvinkel \delta rad"
+hold on
+plot(x, z1, 'b-')
+plot(x, z2, 'g-')
+plot(x, z3, 'y-')
+plot(x, z4, 'r-')
+hold off
