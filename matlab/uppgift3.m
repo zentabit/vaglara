ef89ca75c76dbaa46f8692170ee016fba26c7b61
Emil Babayev <zentabit@gmail.com>
2020-04-29 10:34:12 +0200

    Update uppg3

--- matlab/uppgift3.m
+++ matlab/uppgift3.m
@@ -1,6 +1,7 @@
 n = 100;
 S = zeros(1, n)
-spalts = 2;
+
+spalts = 5;
 % fler spalter = fler v?gor som interfererar
 width = 2;
 % bredare spalt = v?gen interfererar med sig sj?lv?
@@ -12,14 +13,31 @@ end
 
 X = fft(S);
 
-f = (0:n-1);
-pow = abs(X.^2);
+% Ber?kna utan att skifta frekvenserna
+%f = (0:n-1);
+%pow = abs(X.^2);
 %plot(f, pow)
 
 Y = fftshift(X);
 fsh = (-n/2:n/2-1);
 pows = abs(Y.^2);
+
 hold on
-plot(fsh, pows)
-plot(fsh, S.*10)
-%axis([ -10 10 0 20]);
+no = pows/norm(pows);
+plot(fsh,no)
+plot(fsh, S)
+
+%% extra
+d = space;
+b = width;
+N = 3;
+lambda = 500;
+i0 = 1;
+theta = linspace(-pi/6, pi/6);
+beta = pi./lambda.*b.*sin(theta);
+gamma = pi./lambda.*d.*sin(theta);
+
+intensity = i0.*(sin(beta)./beta).^2.*(sin(N.*gamma)/(sin(gamma))).^2;
+
+no2 = intensity/norm(intensity)
+plot(theta, no2)
