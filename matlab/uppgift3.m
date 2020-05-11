9391174de49ab93b935effb6eb120f85bc0c48d3
Emil Babayev <zentabit@gmail.com>
2020-05-11 09:53:27 +0200

    nicen up labb3

--- matlab/uppgift3.m
+++ matlab/uppgift3.m
@@ -3,7 +3,7 @@ t = linspace(0, 4*pi, 1000);
 x = zeros(1, length(t));
 y = zeros(1, length(t));
 z = zeros(1, length(t));
-maxn = 1003;
+maxn = 10001;
 
 for i = 1:2:maxn
     x = x + 1/i*sin(i.*t);
@@ -17,53 +17,52 @@ for i = 1:maxn
     z = z + 1/i*sin(i.*t);
 end
 
-hold on
+subplot(2,2,1);
 plot(t,x)
+title("Fyrkant");
+subplot(2,2,2);
 plot(t,y)
+title("Triangel");
+subplot(2,2,3);
 plot(t,z)
+title("S?gtand");
+subplot(2,2,4);
 plot(t, sin(t))
+title("Sinus");
 
 %% del 2
-n = 100;
+% antalet datapunkter
+n = 1024;
 S = zeros(1, n);
 
-spalts = 2;
+spalts = 3;
 % fler spalter = fler v?gor som interfererar
 width = 4;
 % bredare spalt = v?gen interfererar med sig sj?lv?
-space = 10;
+space = 20;
 % mer avst?nd = mer interferens?
+% fyll ut S med spalterna
 for i = 1:spalts
     S(space*i: space*i+width) = 1;
 end
 
 X = fft(S);
 
-% Ber?kna utan att skifta frekvenserna
+% Rita utan att skifta frekvenserna
 %f = (0:n-1);
 %pow = abs(X.^2);
 %plot(f, pow)
 
+% skifta frekvenserna och skapa en vektor med "frekvenserna"
 Y = fftshift(X);
 fsh = (-n/2:n/2-1);
+
 pows = abs(Y.^2);
-
-hold on
+% normera fj?rrf?ltet och plotta
 no = pows/norm(pows);
+subplot(2,1,1);
 plot(fsh,no)
+title("Normaliserat fj?rrf?lt");
+subplot(2,1,2);
 plot(fsh, S)
-
-%% extra
-d = space;
-b = width;
-N = 3;
-lambda = 500;
-i0 = 1;
-theta = linspace(-pi/6, pi/6);
-beta = pi./lambda.*b.*sin(theta);
-gamma = pi./lambda.*d.*sin(theta);
-
-intensity = i0.*(sin(beta)./beta).^2.*(sin(N.*gamma)/(sin(gamma))).^2;
-
-no2 = intensity/norm(intensity)
-plot(theta, no2)
+title("Spalterna");
